9d5990cdc24acc1d8498db09757fd526ce618f12
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-07-21 10:42:37 +0200

    Házifeladat beadása

--- python-homework/tenbelow.py
+++ python-homework/tenbelow.py
@@ -4,7 +4,7 @@ while True:
     my_x = int(input("Kérem a következő számot: "))
     x = x + my_x
     if my_x >= 10:
-        x = x-my_x
+        x = x - my_x
         break
 
 print("A beolvasott egyjegyű számok összege: " + str(x))
