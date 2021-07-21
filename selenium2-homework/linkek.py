806f9edfa74e767404e58c723fcee4ca927744c6
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-07-21 12:12:45 +0200

    Házifeladat beadása

--- selenium2-homework/linkek.py
+++ selenium2-homework/linkek.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
-import os
+
 from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 try:
     driver.get("http://localhost:9999/")
     links = driver.find_elements_by_xpath("//a")
-    for l in links:
+    for link in links:
         with open('linkek.txt', 'a') as linkek:
-            linkek.write(str(l.get_attribute("href"))+'\n')
-        print(l.get_attribute("href"))
+            linkek.write(str(link.get_attribute("href"))+'\n')
+        print(link.get_attribute("href"))
 
     len_links = len(links)
     print(f"Linkek száma: {len_links}")
